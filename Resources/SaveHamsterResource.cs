47b7a62f6a1f2fc4954198b3880fd29cca4bd0f4
Richard Hull <richard.hull@ctt.co.uk>
2019-07-23 11:33:24 +0100

    add age to api post

--- Resources/SaveHamsterResource.cs
+++ Resources/SaveHamsterResource.cs
@@ -5,7 +5,10 @@ namespace MoviesAPI.Resources
     public class SaveHamsterResource
     {
         [Required]
-        [MaxLength(30)]
+        [MaxLength(150)]
         public string Name { get; set; }
+
+        [Required]
+        public string Age { get; set; }
     }
 }
